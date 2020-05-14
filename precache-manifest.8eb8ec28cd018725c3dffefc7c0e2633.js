4ad1d59c7f332ef43635a209c6dd07796b40df0a
Binayak Behera <binayakbehera999@gmail.com>
2020-05-14 22:50:09 +0530

    Updates

---
+++ precache-manifest.8eb8ec28cd018725c3dffefc7c0e2633.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa276eb1a50a694b93234fba27cdf9a",
+    "url": "/mobishaala/index.html"
+  },
+  {
+    "revision": "9206be9d32be1d59562b",
+    "url": "/mobishaala/static/css/2.91c80c38.chunk.css"
+  },
+  {
+    "revision": "393551231237dd50a3ed",
+    "url": "/mobishaala/static/css/main.9041557e.chunk.css"
+  },
+  {
+    "revision": "9206be9d32be1d59562b",
+    "url": "/mobishaala/static/js/2.6f61e3ec.chunk.js"
+  },
+  {
+    "revision": "6d94984a5fdbad217098935a3d45084e",
+    "url": "/mobishaala/static/js/2.6f61e3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393551231237dd50a3ed",
+    "url": "/mobishaala/static/js/main.c4d79a15.chunk.js"
+  },
+  {
+    "revision": "a7abb6d13e6e726224f3",
+    "url": "/mobishaala/static/js/runtime-main.ec9a8ad1.js"
+  },
+  {
+    "revision": "8daf8ff8dc62c63ba4d1dc5636c53a44",
+    "url": "/mobishaala/static/media/c1.8daf8ff8.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mobishaala/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mobishaala/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mobishaala/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mobishaala/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mobishaala/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aae0b25e5a95849d344bb9267c3a32d4",
+    "url": "/mobishaala/static/media/logo.aae0b25e.png"
+  },
+  {
+    "revision": "3529aeccd7c7f3c117a1d05bc3e4c12e",
+    "url": "/mobishaala/static/media/playstore.3529aecc.png"
+  },
+  {
+    "revision": "74a01df799cd3b1414d9a35e4a9ce329",
+    "url": "/mobishaala/static/media/screen.74a01df7.png"
+  }
+]);
